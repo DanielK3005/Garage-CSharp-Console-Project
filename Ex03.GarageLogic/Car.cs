05db1994a94d59552d572570c2baa992986ea7f2
itsme <itsme@LIOR>
2023-05-14 21:19:33 +0300

    update

---
+++ Ex03.GarageLogic/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Car:Vehicle
+    {
+        private eCarColor m_CarColor;
+        private eNumberOfDoors m_NumberOfDoors;
+        public enum eCarColor
+        {
+            White,
+            Black,
+            Yellow,
+            Red
+        }
+        public enum eNumberOfDoors
+        {
+            Two,
+            Three,
+            Four,
+            Five
+        }
+
+    }
+}
--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -8,25 +8,19 @@ namespace Ex03.GarageLogic
 {
     internal class Vehicle
     {
-        private string m_ModelName;
         private string m_LicenseNumber;
-        private float m_EnergyPercentage;
         private List<Wheel> m_Wheels;
         private string m_OwnerName;
         private string m_OwnerPhoneNumber;
         private eVehicleStatus m_VehicleStatus;
-        private eVehicleType m_VehicleType;
 
-        public Vehicle(string modelName, string licenseNumber, float energyPercentage, List<Wheel> wheels, string ownerName, string ownerPhoneNumber, eVehicleStatus vehicleStatus, eVehicleType vehicleType)
+        public Vehicle(string i_ModelName, string i_LicenseNumber, float i_EnergyPercentage, List<Wheel> i_Wheels, string i_OwnerName, string i_OwnerPhoneNumber, eVehicleStatus i_VehicleStatus)
         {
-            m_ModelName = modelName;
-            m_LicenseNumber = licenseNumber;
-            m_EnergyPercentage = energyPercentage;
-            m_Wheels = wheels;
-            m_OwnerName = ownerName;
-            m_OwnerPhoneNumber = ownerPhoneNumber;
-            m_VehicleStatus = vehicleStatus;
-            m_VehicleType = vehicleType;
+            m_LicenseNumber = i_LicenseNumber;
+            m_Wheels = i_Wheels;
+            m_OwnerName = i_OwnerName;
+            m_OwnerPhoneNumber = i_OwnerPhoneNumber;
+            m_VehicleStatus = i_VehicleStatus;
         }
 
         public enum eVehicleStatus
@@ -36,23 +30,13 @@ namespace Ex03.GarageLogic
             Paid
         }
 
-        //enum vehicle type regular or electric
-        public enum eVehicleType
-        {
-            Regular,
-            Electric
-        }
-
         public virtual string DisplayInformation()
         {
             StringBuilder vehicleInformation = new StringBuilder();
-            vehicleInformation.AppendFormat("Model name: {0}", m_ModelName);
             vehicleInformation.AppendFormat("License number: {0}", m_LicenseNumber);
-            vehicleInformation.AppendFormat("Energy percentage: {0}", m_EnergyPercentage);
             vehicleInformation.AppendFormat("Owner name: {0}", m_OwnerName);
             vehicleInformation.AppendFormat("Owner phone number: {0}", m_OwnerPhoneNumber);
             vehicleInformation.AppendFormat("Vehicle status: {0}", m_VehicleStatus);
-            vehicleInformation.AppendFormat("Vehicle type: {0}", m_VehicleType);
             return vehicleInformation.ToString();
         }
     }
