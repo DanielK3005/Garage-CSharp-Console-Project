7f61a150c2a3b2707ec85c1d7e85f74b78faba0f
itsme <itsme@LIOR>
2023-05-16 20:30:24 +0300

    added fuel systems

---
+++ Ex03.GarageLogic/ElectricPowered.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class ElectricPowered : VehiclePowerSystem
+    {
+        private const string k_EnergyType = "Battery Hour";
+
+        public ElectricPowered(float i_CurrentBatteryTime, float i_MaxBatteryTime)
+        {
+            m_CurrentEnergyAmount = i_CurrentBatteryTime;
+            m_MaxEnergyAmount = i_MaxBatteryTime;
+        }
+
+        public void Recharge(float i_BatteryTimeAmount)
+        {
+            if (!AddEnergy(i_BatteryTimeAmount))
+            {
+                throw new EnergyCapacityException(k_EnergyType);
+            }
+        }
+    }
+}
---
+++ Ex03.GarageLogic/InternalCombustionPowered.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class InternalCombustionPowered:VehiclePowerSystem
+    {
+        private const string k_EnergyType = "Fuel";
+
+        public enum eFuelType
+        {
+            Soler,
+            Octan95,
+            Octan96,
+            Octan98
+        }
+
+        private eFuelType m_FuelType;
+
+        public InternalCombustionPowered(eFuelType i_FuelType, float i_CurrentFuelAmount, float i_MaxFuelAmount)
+        {
+            m_CurrentEnergyAmount = i_CurrentFuelAmount;
+            m_MaxEnergyAmount = i_MaxFuelAmount;
+            m_FuelType = i_FuelType;
+        }
+
+        public void Refuel(float i_FuelAmount, eFuelType i_FuelType)
+        {
+            if (m_FuelType == i_FuelType)
+            {
+                if (!AddEnergy(i_FuelAmount))
+                {
+                    throw new EnergyCapacityException("Fuel");
+                }
+            }
+            else
+            {
+                throw new NoMatchingFuelException();
+            }
+        }
+
+
+
+    }
+}
